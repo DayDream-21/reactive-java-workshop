aa4917fa8506f82d4853d4d98edcf3bba8322b26
DayDream-21 <vova13158@gmail.com>
2023-01-02 23:28:55 +0300

    Complete exercise 6

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -1,23 +1,35 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 public class Exercise6 {
-
-
     public static void main(String[] args) throws IOException {
-
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        // TODO: Write code here
+        String response = ReactiveSources
+                .unresponsiveMono()
+                .timeout(Duration.of(5, ChronoUnit.SECONDS))
+                .onErrorResume(TimeoutException.class, e -> Mono.just("Error"))
+                .block();
+        System.out.println(response);
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        List<String> list = new ArrayList<>();
+        ReactiveSources.unresponsiveFlux()
+                .timeout(Duration.ofSeconds(5))
+                .map(s -> list.add(s))
+                .subscribe();
 
         System.out.println("Press a key to end");
         System.in.read();
     }
-
 }
